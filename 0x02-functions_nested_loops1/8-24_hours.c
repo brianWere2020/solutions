67ba9dba9e0f7d8647ac3c4f844f97e716a2152c
brianWere2020 <mcbrian70@gmail.com>
2023-02-21 22:28:38 +0300

    solutions

---
+++ 0x02-functions_nested_loops1/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "ALX.h"
+/**
+ * It is a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * Prototype: void jack_bauer(void);
+ * You can listen to the soundtrack while coding :)
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour< 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
